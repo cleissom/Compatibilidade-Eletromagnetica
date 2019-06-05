6b4ca25d29a515cffe5b8f1d6d44a4398eb28716
Cleissom <cleissomfernandes@hotmail.com>
2019-06-05 09:13:54 -0300

    added global values and added gif export

--- gerar_videos.bat
+++ gerar_videos.bat
@@ -4,3 +4,9 @@ ffmpeg -r 30 -i tmp/frequencia2_cima_Z/movie%%d.png -c:v libx264 -vf fps=25 -pix
 ffmpeg -r 30 -i tmp/frequencia2_baixo_Z/movie%%d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p exports/videos/frequencia2_baixo_Z.mp4
 ffmpeg -r 30 -i tmp/frequencia1_cima_H/movie%%d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p exports/videos/frequencia1_cima_H.mp4
 ffmpeg -r 30 -i tmp/frequencia1_baixo_H/movie%%d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p exports/videos/frequencia1_baixo_H.mp4
+ffmpeg -i exports/videos/frequencia1_cima_Z.mp4 exports/gifs/frequencia1_cima_Z.gif
+ffmpeg -i exports/videos/frequencia1_baixo_Z.mp4 exports/gifs/frequencia1_baixo_Z.gif
+ffmpeg -i exports/videos/frequencia2_cima_Z.mp4 exports/gifs/frequencia2_cima_Z.gif
+ffmpeg -i exports/videos/frequencia2_baixo_Z.mp4 exports/gifs/frequencia2_baixo_Z.gif
+ffmpeg -i exports/videos/frequencia1_cima_H.mp4 exports/gifs/frequencia1_cima_H.gif
+ffmpeg -i exports/videos/frequencia1_baixo_H.mp4 exports/gifs/frequencia1_baixo_H.gif
